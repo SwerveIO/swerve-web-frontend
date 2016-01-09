c24da989d8636c82bbf61feb6a685439f4c9ec68
Mike Mclaren <mike@bookallu.com>
2016-01-09 14:18:04 -0600

    Added boilerplate code.

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,38 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React, { Component, PropTypes } from 'react';
+import cx from 'classnames';
+import s from './Navigation.scss';
+import withStyles from '../../decorators/withStyles';
+import Link from '../Link';
+
+@withStyles(s)
+class Navigation extends Component {
+
+  static propTypes = {
+    className: PropTypes.string,
+  };
+
+  render() {
+    return (
+      <div className={cx(s.root, this.props.className)} role="navigation">
+        <Link className={s.link} to="/about">About</Link>
+        <Link className={s.link} to="/contact">Contact</Link>
+        <span className={s.spacer}> | </span>
+        <Link className={s.link} to="/login">Log in</Link>
+        <span className={s.spacer}>or</span>
+        <Link className={cx(s.link, s.highlight)} to="/register">Sign up</Link>
+      </div>
+    );
+  }
+
+}
+
+export default Navigation;
